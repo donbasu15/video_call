f622b552e235f7ba1dc8ca0c4019077140531d19
Don Basumatary <donbasty375@gmail.com>
2025-03-28 13:47:29 +0530

    sockets done

--- backend/src/controllers/socket.js
+++ backend/src/controllers/socket.js
@@ -5,7 +5,14 @@ let messages = {};
 let timeOnline = {};
 
 const connectToSocket = (server) => {
-  const io = new Server(server);
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["*"],
+      credentials: true,
+    },
+  });
   io.on("connection", (socket) => {
     socket.on("join-call", (path) => {
       if (connections[path] === undefined) {
@@ -48,14 +55,42 @@ const connectToSocket = (server) => {
           messages[matchingRoom] = [];
         }
         messages[matchingRoom].push({
-          "sender": sender,
-          "data": data,
+          sender: sender,
+          data: data,
           "socket-id-sender": socket.id,
+        });
+        console.log("message", key, ":", sender, data);
+        connections[matchingRoom].forEach((elem) => {
+          io.to(elem).emit("chat-messages", data, sender, socket.id);
         });
       }
     });
 
-    socket.on("disconnect", () => {});
+    socket.on("disconnect", () => {
+      var diffTime = Math.abs(timeOnline[socket.id] - new Date());
+
+      var key;
+
+      for (const [k, v] of JSON.parse(
+        JSON.stringify(Object.entries(connections))
+      )) {
+        for (let a = 0; a < v.length; a++) {
+          if (v[a] == socket.id) {
+            key = k;
+            for (let a = 0; a < connections[key].length; a++) {
+              io.to(connections[key][a]).emit("user-left", socket.id);
+            }
+            var index = connections[key].indexOf(socket.id);
+
+            connections[key].splice(index, 1);
+
+            if (connections[key].length === 0) {
+              delete connections[key];
+            }
+          }
+        }
+      }
+    });
   });
   return io;
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { login, register } from "../controllers/user.controller.js";
 
-
-const userRoutes = Router()
+const userRoutes = Router();
 
 userRoutes.route("/login").post(login);
 userRoutes.route("/register").post(register);
-userRoutes.route("/add_to_activity")
-userRoutes.route("get_activity")
+userRoutes.route("/add_to_activity");
+userRoutes.route("/get_activity");
 
 export default userRoutes;
