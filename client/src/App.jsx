74afeda53961849ef4b52b6cf917d7b06c15e39f
Don Basumatary <donbasty375@gmail.com>
2025-03-30 18:51:12 +0530

    landing page --done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,19 @@
-import { useRef, useState } from 'react'
-import './App.css'
-
-
-
-
+import { useRef, useState } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import LandingPage from "./pages/landing";
 
 function App() {
-  const [stream,setStream] = useState(null);
-
-  const webcamVideo = useRef()
-  const startStream = async () => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: true,
-        audio: true,
-      })
-      .then(newStream => {
-        console.log("Got media",newStream);
-        webcamVideo.current.srcObject = newStream;
-        setStream(newStream);
-      });
-  
-  };
-  const stopStream = () => {
-    stream.getTracks().forEach((track) => track.stop());
-
-    setPlaying(false);
-};
   return (
     <>
-      <video ref={webcamVideo} autoPlay playsInline></video>
-      <button onClick={startStream}>Start</button>
-      <button onClick={stopStream}>Stop</button>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/home" element=""/> */}
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/landing.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+export default function LandingPage() {
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Apna Video Call</h2>
+        </div>
+        <div className="navlist">
+          <p>Join as Guest</p>
+          <p>Register</p>
+          <div role="button">
+            <p>Login</p>
+          </div>
+        </div>
+      </nav>
+      <div className="landingMainContainer">
+        <div className="main-message">
+          <h1>
+            <span style={{ color: "#FF9" }}>Connect</span> with your loved ones.
+          </h1>
+          <p>Cover a distance by apna video call.</p>
+          <button>
+            <Link to={"/home"}>Get Started</Link>
+          </button>
+        </div>
+        <div className="mobile-img">
+          <img src="/mobile.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
